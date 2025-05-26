59150a26a03fc117328b7384297c84345182692c
R1cecracker <troy.harcoan0@gmail.com>
2025-05-26 01:30:26 +0000

    Completed up until challenge 13

--- cave.py
+++ cave.py
@@ -1,18 +1,13 @@
 class Cave():
     def __init__(self, type):
         self.type = type
-        self.info = None
+        self.description = None
         self.linked_caves = {}
     
-    def get_info(self):
-        return self.info
+    def get_description(self):
+        return self.description
     
-    def set_info(self, info):
-        self.info = info
+    def set_description(self, description):
+        self.description = description
 
-    def get_type(self):
-        return self.type
-
-    def info(self):
-        print({self.type})
-
+    
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from cave import Cave
 
+cavern = Cave("cavern")
+grotto = Cave("grotto")
+dungeon = Cave("Dungeon")
+cavern.set_description("A damp and dirty cave.")
+cavern.get_description()
